44a49b597703437ecea01f2125ee883707775e7f
daniswastaken <daniswastaken@gmail.com>
2025-06-18 18:30:23 +0700

    trying user input on HTML with JS

---
+++ js/core.js
@@ -0,0 +1,9 @@
+let distance;
+
+document.getElementById("calculate").onclick = function() {
+    distance = document.getElementById("distance").value;
+    if (distance = 1515) {
+        window.alert("It works.");
+    }
+}
+
